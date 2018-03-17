112ee234b0f1c35d0532bd66c1f0db60f237657c
ssm1998 <32703974+ssm1998@users.noreply.github.com>
2018-03-17 12:43:00 +0530

    Created Start Page

--- src/Start.js
+++ src/Start.js
@@ -1,42 +1,54 @@
 import React,  { Component } from 'react';
-import { StyleSheet, View, Image, Text, TextInput, TouchableOpacity } from 'react-native';
+import { Alert, StyleSheet, View, ImageBackground, Text, Button } from 'react-native';
 
 export default class Login extends Component {
+  _onPressButton() {
+    Alert.alert('Navigate to next page!')
+  }
+
     render() {
         return (
-            <View style = {styles.container}>
-                <Image style = {styles.background} source={require('./StartPageImg.jpg')}/>
-                <View style = {styles.buttons}>
-                  <TouchableOpacity style = {styles.buttonContainer}>
-                    <Text style = {styles.buttonText}> LOGIN </Text>
-                  </TouchableOpacity>
-                  <TouchableOpacity style = {styles.buttonContainer}>
-                    <Text style = {styles.buttonText}> SIGN UP </Text>
-                  </TouchableOpacity>
-
-                </View>
-            </View>
+          <View style = {styles.container}>
+            <ImageBackground style = {styles.background} source = {require('./StartPageImgFade.jpg')}>
+              <View style = {styles.buttonContainer}>
+                <Button
+                  onPress = {this._onPressButton}
+                  containerStyle = {{padding:10, height:45, overflow:'hidden', borderRadius:100, backgroundColor: 'rgba(234, 202, 251, 0.8)'}}
+                  disabledContainerStyle = {{backgroundColor: 'grey'}}
+                  style = {{fontSize: 20, color: 'white', backgroundColor: 'rgba(233, 47, 109, 0.8)'}}
+                  title = "LOGIN">
+                  
+                </Button>
+              </View>
+              <View style = {styles.buttonContainer}>
+                <Button
+                    onPress = {this._onPressButton}
+                    containerStyle = {{padding:10, height:45, overflow:'hidden', borderRadius:100, backgroundColor: 'rgba(234, 202, 251, 0.8)'}}
+                    disabledContainerStyle = {{backgroundColor: 'grey'}}
+                    style = {{fontSize: 20, color: 'white', backgroundColor: 'rgba(233, 47, 109, 0.8)'}}
+                    title = "SIGN UP">
+              
+                </Button>
+              </View>
+          </ImageBackground>
+        </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-    },
-    background: {
-        flex: 1
-    },
-    buttons: {
-      padding: 20,
-    },
-    buttonContainer: {
-      backgroundColor: 'rgba(255,251,255,0.8)',
-      padding: 10
-    },
-    buttonText: {
-      textAlign: 'center',
-      fontWeight: '700'
-    }
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+   },
+   background: {
+    flex: 1,
+    justifyContent: 'center',
+   },
+   buttonContainer: {
+     margin: 20,
+     borderRadius:100,
+     backgroundColor: 'rgba(233, 47, 109, 0.8)',
+   },
 
 });
