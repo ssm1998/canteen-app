de4b1582707f3f954840e54a13618df5394c0bbf
gurleen-kaur-30 <32809861+gurleen-kaur-30@users.noreply.github.com>
2018-03-20 23:40:37 +0530

    Update SignUp1.js

--- src/SignUp1.js
+++ src/SignUp1.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, Image, View } from 'react-native';
+//import FitImage from 'react-native-fit-image';
+import { StyleSheet, Text,TextInput, Image, View,TouchableOpacity,ImageBackground } from 'react-native';
+//const remote = "https://www.nautilusplus.com/healthy-alternatives-junk-food/"
+//const width = Dimensions.get('window').width
 export default class App extends React.Component {
   render() {
     return (
-      <View>
+   
+    
+      <ImageBackground style = {{flex:1, backgroundColor: 'rgba(0, 0, 0,0.2 )'
+}} source={require('./StartPageImg.jpg')}>
+     
 	  <Image
-	  	style={{width:400, height: 100,marginTop:50,
+	  	style={{width:400, height: 100,resizeMode:'center',marginTop:50,
 	
 		justifyContent: 'center', alignSelf:'center'}}
+		//alignItems: 'center',
 		
 		source={require('./are3.png')}
 	
@@ -15,25 +23,38 @@ export default class App extends React.Component {
 	
 
 	<View style={{flexDirection:'row'}}>
+
+     <TouchableOpacity activeOpacity = { .5} onEnter={()=>console.log("onEnter!")}>
 	  <Image
-	   style={{width:200, height: 400,alignSelf:'flex-start',flexWrap:'wrap',marginTop:20}}
+	   style={{width:240, height: 320,alignSelf:'flex-start',flexWrap:'wrap',marginTop:80,}}
 	  source={require('./vendor-clipart-1.jpg')}
 	  
 	  />
+    </TouchableOpacity>	 
 
+   
 	  
 
 	 
-
+      <TouchableOpacity activeOpacity = { .5 } onEnter={()=>console.log("onEnter!")}>
 	  <Image
-	  style={{width:200, height: 400,alignSelf:'flex-end',flexWrap:'wrap',marginLeft:12,marginTop:20}}
-	  source={require('./customer.jpg')}
+	  style={{width:200, height: 320,alignSelf:'flex-end',flexWrap:'wrap',marginLeft:0,marginTop:50}}
+	  source={require('./consumer.png')}
 	  />
+          </TouchableOpacity>
+
+      <TextInput style={{height:100}} value={'hello'}/>
+      
+      	 
+	  </View>
+      
+      </ImageBackground>
 	  
-	  </View>
-	  </View>
+	  
+    
     );
   }
 }
+
 
 
